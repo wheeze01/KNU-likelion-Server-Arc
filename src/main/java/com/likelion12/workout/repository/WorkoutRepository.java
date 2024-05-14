4d9b411c8fb5a1185fe07832e483b64598e75161
freshman0101 <54202163+wheeze01@users.noreply.github.com>
2024-05-14 19:17:28 +0900

    first commit

---
+++ src/main/java/com/likelion12/workout/repository/WorkoutRepository.java
@@ -0,0 +1,9 @@
+package com.likelion12.workout.repository;
+
+import com.likelion12.workout.model.Workout;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WorkoutRepository extends JpaRepository<Workout, Long> {
+}
